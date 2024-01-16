3d7c4a9db8a916fe9c9e1cc32c467153f1107f11
UMTRanger <149034825+UMTRanger@users.noreply.github.com>
2024-01-16 10:40:04 +0800

    Delete CSF3133 G13 Assignment directory

--- CSF3133 G13 Assignment/src/radio_button.js
+++
@@ -1,7 +0,0 @@
-function change(radio) {
-    if (radio.checked && radio.id === "radio-restaurant") {
-        document.getElementById("button").innerHTML = "<a href='./restaurant.html'>Submit</a>";
-    } else {
-        document.getElementById("button").innerHTML = "<a href='./hotel.html'>Submit</a>"
-    }
-}
